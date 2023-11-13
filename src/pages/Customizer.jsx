736d70634813e71248fcd878ddbfe5b97056c0c8
Johan Yindou <jyindou@gmail.com>
2023-11-13 17:00:22 +0100

    Supakar v1.0

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -6,6 +6,8 @@ import carGreen from '../assets/img/green.png'
 import carYellow from '../assets/img/yellow.png'
 import Nav from '../components/Navbar'
 
+
+
 function Customizer() {
   const [color, setColor] = useState([carOriginal])
 
@@ -34,7 +36,9 @@ function Customizer() {
     <>
       <Nav />
       <main className='customizer'>
+        <a href={ color } className='glightbox'>
         <Car color={color} />
+        </a>
         <div className="options">
           <h1>Personalisation</h1>
           <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente.</p>
